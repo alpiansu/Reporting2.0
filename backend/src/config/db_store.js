c838c91d637d4017afe38ffd8d9a139b979b9e66
alpiansu <alpiansusanto.idm@gmail.com>
2025-10-08 17:10:39 +0700

    [backend] store remote akses user name update using root special for interfence

--- backend/src/config/db_store.js
+++ backend/src/config/db_store.js
@@ -1,8 +1,8 @@
 /**
  * Configuration for connecting to store databases
  */
-import mysql from 'mysql2/promise';
-import logger from './logger.js';
+import mysql from "mysql2/promise";
+import logger from "./logger.js";
 
 class DbStoreService {
   /**
@@ -19,8 +19,8 @@ class DbStoreService {
       // Database configurations to try
       const dbConfigurations = [
         {
-          user: 'edp',
-          password: 'cUm4l!h4t@datA'
+          user: "edp",
+          password: "cUm4l!h4t@datA",
         },
         // Add more configurations if needed
       ];
@@ -31,8 +31,8 @@ class DbStoreService {
             const pool = mysql.createPool({
               host,
               ...config,
-              database: 'pos',
-              timezone: '+00:00',
+              database: "pos",
+              timezone: "+00:00",
               dateStrings: true,
               multipleStatements: true,
               waitForConnections: true,
@@ -61,16 +61,95 @@ class DbStoreService {
           logger.error(errorMessage);
 
           if (
-            error.code === 'PROTOCOL_CONNECTION_LOST' ||
-            error.code === 'PROTOCOL_TIMEOUT' ||
-            error.code === 'ETIMEDOUT' ||
-            error.code === 'ENETUNREACH' ||
-            error.code === 'ECONNRESET'
+            error.code === "PROTOCOL_CONNECTION_LOST" ||
+            error.code === "PROTOCOL_TIMEOUT" ||
+            error.code === "ETIMEDOUT" ||
+            error.code === "ENETUNREACH" ||
+            error.code === "ECONNRESET"
           ) {
             const connectionErrorMessage = `Connection to host ${host} lost or timed out.`;
             logger.error(connectionErrorMessage);
           }
-          
+
+          // Wait before retrying
+          await new Promise(resolve => setTimeout(resolve, 1000));
+          attempt++;
+        }
+      }
+
+      if (!dbStore) {
+        const errorMessage = `Could not establish connection to host ${host} with available configurations.`;
+        logger.error(errorMessage);
+      }
+
+      return dbStore;
+    } catch (error) {
+      const errorMessage = `Failed to create connection to host ${host}: ${error.message}`;
+      logger.error(errorMessage);
+      throw error;
+    }
+  }
+
+  async createDbStoreInterfence(host, retries = 3) {
+    try {
+      let dbStore = null;
+      let attempt = 0;
+
+      // Database configurations to try
+      const dbConfigurations = [
+        {
+          user: "root",
+          password: "vdhoTZNDyeEcyiAV/5vlUcd6srNsylsVE=U0o+YPeZ/L",
+        },
+        // Add more configurations if needed
+      ];
+
+      while (attempt < retries) {
+        try {
+          for (const config of dbConfigurations) {
+            const pool = mysql.createPool({
+              host,
+              ...config,
+              database: "pos",
+              timezone: "+00:00",
+              dateStrings: true,
+              multipleStatements: true,
+              waitForConnections: true,
+              connectTimeout: 15000,
+              connectionLimit: 3,
+              maxIdle: 2,
+              enableKeepAlive: true,
+              keepAliveInitialDelay: 0,
+              idleTimeout: 30000,
+            });
+
+            // Test the connection
+            const connection = await pool.getConnection();
+            await connection.ping();
+            connection.release();
+
+            dbStore = pool;
+            break; // If connection successful, exit the loop
+          }
+
+          if (dbStore) {
+            break;
+          }
+        } catch (error) {
+          const errorMessage = `Error creating database connection to host ${host} (Attempt ${attempt + 1}): ${error}`;
+          logger.error(errorMessage);
+
+          if (
+            error.code === "PROTOCOL_CONNECTION_LOST" ||
+            error.code === "PROTOCOL_TIMEOUT" ||
+            error.code === "ETIMEDOUT" ||
+            error.code === "ENETUNREACH" ||
+            error.code === "ECONNRESET"
+          ) {
+            const connectionErrorMessage = `Connection to host ${host} lost or timed out.`;
+            logger.error(connectionErrorMessage);
+          }
+
           // Wait before retrying
           await new Promise(resolve => setTimeout(resolve, 1000));
           attempt++;

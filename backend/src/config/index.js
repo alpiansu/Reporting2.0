15a652555185ad8fecc14eca7d83f7cc42153641
alpiansu <alpiansusanto.idm@gmail.com>
2026-09-07 11:46:01 +0700

    update index config untuk penambahan whitelist dan menghapus comment tidak perlu

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-import resilientDb from './resilient-database.js';
+import resilientDb from "./resilient-database.js";
 import jwt from "./jwt.js";
 import logger from "./logger.js";
 
@@ -12,7 +12,13 @@ const __dirname = path.dirname(__filename);
 // Load environment variables
 dotenv.config({ path: path.join(__dirname, "../../.env") });
 
-const allowedOrigins = ["http://localhost", "http://192.168.61.228", "http://192.168.133.10", "http://127.0.0.1"];
+const allowedOrigins = [
+  "http://localhost",
+  "http://192.168.61.228",
+  "http://192.168.133.10",
+  "http://127.0.0.1",
+  "http://192.168.133.7",
+];
 
 export default {
   // Server configuration
@@ -21,22 +27,14 @@ export default {
 
   // Database configuration
   database: resilientDb.config,
-
-  // JWT configuration
   jwt,
-
-  // Logger configuration
   logger,
-
-  // Connection timeout
   connectionTimeout: parseInt(process.env.CONNECTION_TIMEOUT) || 30000,
 
   // CORS configuration
   corsOptions: {
     origin: (origin, callback) => {
       if (!origin) return callback(null, true); // allow non-browser tools (Postman, curl, etc)
-      // console.log("origin", origin); // Disabled unnecessary logging
-      // cek apakah origin ada di whitelist (mulai dengan localhost atau 192.168.61.228)
       if (allowedOrigins.some(allowed => origin.startsWith(allowed))) {
         return callback(null, true);
       } else {
@@ -50,6 +48,5 @@ export default {
     credentials: true,
   },
 
-  // Database instance
   resilientDb,
 };
